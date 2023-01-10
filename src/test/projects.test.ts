de7066043cdbd69064c35eabddb6e33a344de850
silvaej <esilva@fullscale.io>
2023-01-10 20:55:31 +0800

    added tests for the /projects route

--- src/test/projects.test.ts
+++ src/test/projects.test.ts
@@ -8,54 +8,52 @@ beforeAll(async () => {
     server = await getServer()
 })
 
-describe('POST /tasks', () => {
-    it('should return status 201', async () => {
+describe('POST /projects', () => {
+    it('should return status 201 and an id', async () => {
         const payload = {
-            test_id: 'test-001',
-            name: 'TEST_0001',
+            test_id: 'test_001',
+            title: 'PROJECT_TEST_001',
             description: 'Some description :)))',
-            status: 'TO_DO',
         }
-        const res = await request(server).post('/tasks').send(payload)
+        const res = await request(server).post('/projects').send(payload)
         expect(res.statusCode).toEqual(201)
         expect(res.body).toHaveProperty('id')
     })
 })
 
-describe('GET /tasks', () => {
-    it('should return status 200 and expect an array of TASKS', async () => {
-        const res = await request(server).get('/tasks')
+describe('GET /projecs', () => {
+    it('should return status 200 and expect an array of Projects', async () => {
+        const res = await request(server).get('/projects')
         expect(res.statusCode).toEqual(200)
         expect(res.body).toBeInstanceOf(Array)
     })
 })
 
-describe('GET /tasks/:id', () => {
+describe('GET /projects/:id', () => {
     it('should return status 200 and receive an instance of task', async () => {
-        const res = await request(server).get('/tasks/test-001')
+        const res = await request(server).get('/projects/test-001')
         expect(res.statusCode).toEqual(200)
         expect(res.body).toHaveProperty('test_id', 'test-001')
         expect(res.body).toHaveProperty('name')
         expect(res.body).toHaveProperty('description')
-        expect(res.body).toHaveProperty('status')
+        expect(res.body).toHaveProperty('tasks', [])
+        expect(res.body).toHaveProperty('dateCreated')
     })
 })
 
-describe('PUT /tasks/:id', () => {
+describe('PUT /projects/:id', () => {
     it('should return status 204', async () => {
         const payload = {
-            name: 'TEST_0001-EDITED',
-            description: 'Some description :)))',
-            status: 'TO_DO',
+            title: 'PROJECT_TEST_001_EDITED',
         }
-        const res = await request(server).put('/tasks/test-001').send(payload)
+        const res = await request(server).put('/projects/test-001').send(payload)
         expect(res.statusCode).toEqual(204)
     })
 })
 
-describe('DELETE /tasks/:id', () => {
+describe('DELETE /projects/:id', () => {
     it('should return status 204', async () => {
-        const res = await request(server).delete('/tasks/test-001')
+        const res = await request(server).delete('/projects/test-001')
         expect(res.statusCode).toEqual(204)
     })
 })
